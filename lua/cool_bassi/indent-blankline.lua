7fbbab6e80908fd19d1be161e3a26986b797bf97
César Miran <csarmiran@gmail.com>
2022-09-04 17:07:06 +0200

    feat!: add indent-blankline

---
+++ lua/cool_bassi/indent-blankline.lua
@@ -0,0 +1,26 @@
+local status_ok, indent_blankline = pcall(require, "indent_blankline")
+if not status_ok then
+  return
+end
+
+indent_blankline.setup {
+  filetype_exclude = {
+    "checkhealth",
+    --[[ "dashboard", ]]
+    "help",
+    "lspinfo",
+    "man",
+    "NvimTree",
+    "packer",
+    "startify",
+    "",
+    --[[ "neogitstatus", ]]
+    --[[ "text", ]]
+    --[[ "Trouble", ]]
+  },
+  use_treesitter = true,
+  use_treesitter_scope = true,
+  show_first_indent_level = false,
+  show_trailing_blankline_indent = false,
+}
+

9f1cf9793cbd8ba99cc92859c9460e59caf5a9ac
César Miran <cesar@MacBook-Pro-de-Cesar.local>
2022-11-15 20:02:34 +0100

    chore(lsp): update golangci_lint_ls cfg

--- lua/cool_bassi/lsp/settings/golangci_lint_ls.lua
+++ lua/cool_bassi/lsp/settings/golangci_lint_ls.lua
@@ -1,7 +1,47 @@
+local root_pattern = require("lspconfig").util.root_pattern
+
+
+
 return {
-	-- filetypes = {
-  --   'go',
-  --   'gomod',
+  -- cmd = {
+  --   "golangci-lint-langserver"
+  -- },
+  -- init_options = {
+  --   command = {
+  --     "golangci-lint",
+  --     "run",
+  --     "--enable-all",
+  --     "--out-format",
+  --     "json"
+  --   },
+
+  -- },
+	filetypes = {
+    "go",
+    "gomod",
+  },
+  -- root_dir = root_pattern("go.mod", ".golangci.yaml", ".git"),
+  -- configs = {
+  --   golangcilsp = {
+  --     default_config = {
+  --       cmd = {
+  --         "golangci-lint-langserver"
+  --       },
+  --       root_dir = require("lspconfig").util.root_pattern("go.mod", ".golangci.yaml", ".git"),
+  --       init_options = {
+  --         command = {
+  --           "golangci-lint",
+  --           "run",
+  --           "--enable-all",
+  --           "--disable",
+  --           "lll",
+  --           "--out-format",
+  --           "json",
+  --           "--issues-exit-code=1"
+  --         },
+  --       },
+  --     },
+  --   },
   -- },
 }
 

92573d9914747694ec3215894fa31f1a7c838c0a
César Miran <csarmiran@gmail.com>
2022-11-15 21:16:50 +0100

    feat(colorscheme): add custom setup for nightfox

--- lua/cool_bassi/colorscheme.lua
+++ lua/cool_bassi/colorscheme.lua
@@ -1,8 +1,25 @@
-local colorscheme = "nightfox"
-
-local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+local status_ok, nightfox = pcall(require, 'nightfox')
 if not status_ok then
-  vim.cmd "colorscheme default"
-  -- vim.notify("colorscheme " .. colorscheme .. " not found!")
   return
 end
+
+nightfox.setup {
+  groups = {
+    all = {
+      ColorColumn = { link = "CursorLine" },
+      EndOfBuffer = { fg = "bg1" },
+      FloatBorder = { bg = "bg0" },
+      LspFloatWinBorder = { fg = "bg0" },
+      NvimTreeEndOfBuffer = { fg = "bg0" },
+      NvimTreeWinSeparator = { bg = "bg1", fg = "bg1" },
+      TelescopeBorder = { bg = "bg0", fg = "bg0" },
+      TelescopeNormal = { bg = "bg0" },
+      WinSeparator = { fg = "bg1" },
+      -- WhichKey = { bg = "bg2" },
+    },
+  },
+}
+
+vim.cmd "colorscheme nightfox"
+vim.g.palette = require("nightfox.palette").load("nightfox") 
+

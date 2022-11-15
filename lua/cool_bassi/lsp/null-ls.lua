0433fa77492c10390868396b0c09209af8aee9aa
César Miran <csarmiran@gmail.com>
2022-11-15 22:48:11 +0100

    refactor(null-ls): update cfg

--- lua/cool_bassi/lsp/null-ls.lua
+++ lua/cool_bassi/lsp/null-ls.lua
@@ -1,6 +1,6 @@
 local null_ls_status_ok, null_ls = pcall(require, "null-ls")
 if not null_ls_status_ok then
-	return
+  return
 end
 
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
@@ -9,15 +9,17 @@ local diagnostics = null_ls.builtins.diagnostics
 local formatting = null_ls.builtins.formatting
 
 null_ls.setup({
-	debug = false,
-	sources = {
+  debug = false,
+  sources = {
     diagnostics.standardrb,
-    formatting.standardrb,
-		formatting.stylua,
-    formatting.golangci_lint,
+    diagnostics.golangci_lint,
 
-		--formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
-		--formatting.black.with({ extra_args = { "--fast" } }),
-    -- diagnostics.flake8
-	},
+    formatting.standardrb,
+    formatting.stylua,
+    formatting.gofmt,
+
+    -- diagnostics.flake8,
+    -- formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
+    -- formatting.black.with({ extra_args = { "--fast" } }),
+  },
 })

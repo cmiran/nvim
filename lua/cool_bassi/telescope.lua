35a00d61ec684e1233bde378d1f569f72044f614
César Miran <csarmiran@gmail.com>
2022-11-15 20:30:43 +0100

    feat(telescope): add fzf extension

--- lua/cool_bassi/telescope.lua
+++ lua/cool_bassi/telescope.lua
@@ -88,12 +88,19 @@ telescope.setup {
     -- builtin picker
   },
   extensions = {
-    media_files = {
-        -- filetypes whitelist
-        -- defaults to {"png", "jpg", "mp4", "webm", "pdf"}
-        filetypes = {"png", "webp", "jpg", "jpeg"},
-        find_cmd = "rg" -- find command (defaults to `fd`)
-      }
+    fzf = {
+      fuzzy = true,                    -- false will only do exact matching
+      override_generic_sorter = true,  -- override the generic sorter
+      override_file_sorter = true,     -- override the file sorter
+      case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
+                                       -- the default case_mode is "smart_case"
+    },
+    -- media_files = {
+    --     -- filetypes whitelist
+    --     -- defaults to {"png", "jpg", "mp4", "webm", "pdf"}
+    --     filetypes = {"png", "webp", "jpg", "jpeg"},
+    --     find_cmd = "rg" -- find command (defaults to `fd`)
+    --   }
     -- Your extension configuration goes here:
     -- extension_name = {
     --   extension_config_key = value,
@@ -101,3 +108,5 @@ telescope.setup {
     -- please take a look at the readme of the extension you want to configure
   },
 }
+
+telescope.load_extension('fzf')

a40180ce12da221281eb078fead062f41382416b
Cesar Miran <csarmiran@gmail.com>
2022-11-15 20:02:10 +0100

    chore(lsp): update solargraph cfg

--- lua/cool_bassi/lsp/settings/solargraph.lua
+++ lua/cool_bassi/lsp/settings/solargraph.lua
@@ -1,36 +1,29 @@
 return {
-	settings = {
-    cmd = {
-      "solargraph",
-      "stdio"
+  cmd = {
+    "solargraph",
+    "stdio"
+  },
+  filetypes = {
+    "ruby",
+  },
+  flags = {
+    debounce_text_changes = 150,
+  },
+  init_options = {
+    formatting = true,
+  },
+  root_dir = require("lspconfig").util.root_pattern("Gemfile", ".git"),
+  settings = {
+    solargraph = {
+      -- commandPath = '/usr/local/Cellar/solargraph/0.45.0/bin/solargraph',
+      autoformat = false,
+      completion = true,
+      diagnostic = false,
+      folding = false,
+      references = true,
+      rename = false,
+      symbols = false,
     },
-    filetypes = {
-      "ruby",
-    },
-    flags = {
-      debounce_text_changes = 150,
-    },
-    -- handlers = handlers,
-    init_options = {
-      formatting = false,
-    },
-    -- root_dir = nvim_lsp.util.root_pattern("Gemfile", ".git", "."),
-    root_dir = {
-      "Gemfile",
-      ".git",
-      -- ".",
-    },
-    setting = {
-      solargraph = {
-        autoformat = true,
-        completion = true,
-        diagnostic = true,
-        folding = true,
-        references = true,
-        rename = true,
-        symbols = true
-      }
-    }
   },
 }
 

35f381e527f38d919b74bef1b02c4c7b544f0a4e
César Miran <csarmiran@gmail.com>
2022-11-15 22:37:30 +0100

    refactor(keymaps): update cfg

--- lua/cool_bassi/keymaps.lua
+++ lua/cool_bassi/keymaps.lua
@@ -1,51 +1,35 @@
 local keymap = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
--- local term_opts = { silent = true }
 
 -- remap space as leader key
 keymap("", "<Space>", "<Nop>", opts)
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
--- normal_mode = "n",
--- insert_mode = "i",
--- visual_mode = "v",
--- visual_block_mode = "x",
--- term_mode = "t",
--- command_mode = "c",
-
--- normal
-keymap("n", "F","<cmd>lua vim.lsp.buf.formatting_sync()<CR>", opts) -- format
--- keymap("n", "<Leader>e", "<cmd>lua require'gitsigns'.reset_buffer()<CR>", opts) -- reset buffer to last commit
+-- normal_mode: n
+-- jump to visible line
+keymap("n", "<Down>", "gj", opts)
+keymap("n", "<Up>", "gk", opts)
 -- navigate between windows with arrows 
 keymap("n", "<A-Left>", "<C-w>h", opts)
 keymap("n", "<A-Down>", "<C-w>j", opts)
 keymap("n", "<A-Up>", "<C-w>k", opts)
 keymap("n", "<A-Right>", "<C-w>l", opts)
--- keymap("n", "<M-tab>", "<C-6>", opts)
--- resize windows with <A-kjhl>
+-- resize windows with <A-kjhl> (macos)
 keymap("n", "˙", ":vertical resize +2<CR>", opts)
 keymap("n", "∆", ":resize +2<CR>", opts)
 keymap("n", "˚", ":resize -2<CR>", opts)
 keymap("n", "¬", ":vertical resize -2<CR>", opts)
+-- delete buffer!
+keymap("n", "D", ":Bdelete!<CR>", opts)
 -- navigate buffers
-keymap("n", "L", ":bnext<CR>", opts)
 keymap("n", "H", ":bprevious<CR>", opts)
-keymap("n", "D", ":Bdelete<CR>", opts)
--- move line up and down
--- keymap("n", "J", ":m .+1<CR>==", opts)
--- keymap("n", "K", ":m .-2<CR>==", opts)
-keymap("n", "<Esc>", ":noh<CR>", opts) -- toggle search highlight
--- Nvim-Tree
-keymap("n", "<leader>e", ":NvimTreeToggle<CR>", opts)
--- Telescope
-keymap("n", "<Leader>f", "<cmd>lua require('telescope.builtin').find_files(require('telescope.themes').get_dropdown({previewer=false}))<CR>", opts)
-keymap("n", "<Leader>g", "<cmd>lua require('telescope.builtin').live_grep(require('telescope.themes').get_dropdown({layout_strategy='vertical',layout_config={height=0.80}}))<CR>", opts)
--- keymap("n", "<Leader>gw", "<cmd>lua require('telescope.builtin').grep_string(require('telescope.themes').get_cursor())<CR>", opts)
--- Toggleterm
-keymap("n", "<Leader>lg", ":lua _LAZYGIT_TOGGLE()<CR>", opts)
+keymap("n", "L", ":bnext<CR>", opts)
+-- quit all!
+-- keymap("n", "Q", ":qa!<CR>", opts)
+-- keymap("n", "<M-tab>", "<C-6>", opts)
 
--- visual
+-- visual_mode: v
 -- "tab" line left and right while staying in indent mode
 keymap("v", "<", "<gv", opts)
 keymap("v", ">", ">gv", opts)
@@ -55,18 +39,17 @@ keymap("v", "K", ":m .-2<CR>==", opts)
 keymap("v", "p", '"_dP', opts) -- keep copy past memory
 -- keymap("v", "<Esc>", ":noh<CR>", opts) -- toggle search highlight
 
--- visual block
+-- visual_block_mode: x
 -- move text up and down
 keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
 keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
--- keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
--- keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
--- keymap("x", "<Esc>", ":noh<CR>", opts) -- toggle search highlight
 
--- terminal
+-- term_mode: t
 -- better terminal navigation
 -- keymap("t", "<C-h>", "<C-\\><C-N><C-w>h", term_opts)
 -- keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
 -- keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
 -- keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
 
+-- command_mode: c
+-- insert_mode: i

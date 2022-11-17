f469893df6e4fc6e2b8c0508bece528467938946
César Miran <csarmiran@gmail.com>
2022-11-17 22:42:38 +0100

    style: move packer autocommands

--- lua/cool_bassi/autocommands.lua
+++ lua/cool_bassi/autocommands.lua
@@ -31,3 +31,11 @@ create_autocmd({ "TextYankPost" }, {
 --     ]]
 --   end,
 -- })
+--
+-- Autocommand that reloads neovim whenever you save the plugins.lua file
+-- vim.cmd [[
+--   augroup packer_user_config
+--     autocmd!
+--     autocmd BufWritePost plugins.lua source <afile> | PackerSync
+--   augroup end
+-- ]]

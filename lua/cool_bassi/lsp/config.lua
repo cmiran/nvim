66a69e77586bdfcaab9487777d0532859081840b
César Miran <csarmiran@gmail.com>
2023-01-22 23:08:14 +0100

    feat(lsp): add tsserver

--- lua/cool_bassi/lsp/config.lua
+++ lua/cool_bassi/lsp/config.lua
@@ -10,6 +10,7 @@ local servers = {
   -- "sorbet", -- github.com/sorbet/sorbet
   "sumneko_lua", -- github.com/sumneko/lua-language-server
   "terraformls", -- github.com/hashicorp/terraform-ls
+  "tsserver", -- github.com/typescript-language-server/typescript-language-server
   "tflint", -- github.com/terraform-linters/tflint
   "yamlls", -- github.com/redhat-developer/yaml-language-server
 }
---
+++ lua/cool_bassi/lsp/tsserver.lua
@@ -0,0 +1,16 @@
+return {
+	cmd = { "typescript-language-server", "--stdio" },
+	filetypes = {
+		"javascript",
+		"javascriptreact",
+		"javascript.jsx",
+		"typescript",
+		"typescriptreact",
+		"typescript.tsx",
+	},
+	init_options = {
+		hostInfo = "neovim",
+	},
+	root_dir = require("lspconfig").util.root_pattern("package.json", "tsconfig.json", "jsconfig.json", ".git"),
+	single_file_support = true,
+}

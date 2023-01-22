df883f443503dd147e57ce5ca6b298b997783758
César Miran <csarmiran@gmail.com>
2023-01-22 19:13:30 +0100

    refactor: refine nvim-tree setup

--- lua/cool_bassi/nvim-tree.lua
+++ lua/cool_bassi/nvim-tree.lua
@@ -11,39 +11,32 @@ nvim_tree.setup {
     mappings = {
       list = {
         { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" },
-        { key = "h", cb = tree_cb "close_node" },
+        { key = "w", cb = tree_cb "close_node" },
         { key = "v", cb = tree_cb "vsplit" },
       },
     },
   },
   renderer = {
-    highlight_git = false,
-    root_folder_modifier = ":t", -- :~
+    highlight_git = true,
+    root_folder_modifier = ":t", -- ":~:s?$?/..?"
     indent_markers = {
       enable = true,
+      icons = {
+        corner = "│",
+      },
     },
     icons = {
-      webdev_colors = true,
-      git_placement = "before",
-      padding = " ",
-      symlink_arrow = " ➛ ",
+      -- git_placement = "after",
       show = {
         file = false,
-        folder = true,
-        folder_arrow = true,
-        git = true,
-      },
-      glyphs = {
-        git = {
-          --[[ unstaged = "", ]]
-          --[[ untracked = "U", ]]
-        },
+        folder = false,
+        git = false,
       },
     },
   },
   update_focused_file = {
     enable = true,
-    update_cwd = true,
+    update_root = true,
   },
   ignore_ft_on_setup = {
     "alpha",
@@ -51,6 +44,7 @@ nvim_tree.setup {
   diagnostics = {
     enable = true,
     show_on_dirs = true,
+    debounce_delay = 20,
   },
   actions = {
     open_file = {

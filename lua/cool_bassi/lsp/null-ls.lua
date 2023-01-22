a0e31af6f921ff012f643f89be82e104184c9ce1
César Miran <csarmiran@gmail.com>
2023-01-22 19:15:34 +0100

    refactor(null-ls): remove golangci_lint formatting

--- lua/cool_bassi/lsp/null-ls.lua
+++ lua/cool_bassi/lsp/null-ls.lua
@@ -1,7 +1,9 @@
 local null_ls = require("null-ls")
--- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
+
+-- github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
 -- local diagnostics = null_ls.builtins.diagnostics
--- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
+
+-- github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
 local formatting = null_ls.builtins.formatting
 
 null_ls.setup({
@@ -11,7 +13,6 @@ null_ls.setup({
     -- diagnostics.flake8,
 
     -- formatting.black.with({ extra_args = { "--fast" } }),
-    formatting.golangci_lint,
     -- formatting.standardrb,
     formatting.stylua,
     -- formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),

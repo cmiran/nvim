893070d0c6594f11b0085130a6a743285d18810d
César Miran <csarmiran@gmail.com>
2023-08-28 20:47:09 +0200

    refactor(nvim-notify): resize window according to content

--- lua/plugins/nvim-notify.lua
+++ lua/plugins/nvim-notify.lua
@@ -1,18 +1,18 @@
--- local get_notification_height = function(win)
---   local buf = vim.api.nvim_win_get_buf(win)
---   local height = 0
+local get_notification_height = function(win)
+  local buf = vim.api.nvim_win_get_buf(win)
+  local height = 0
 
---   for _, content in ipairs(vim.api.nvim_buf_get_lines(buf, 0, -1, false)) do
---     local length = string.len(content)
---     if length > 0 then
---       height = height + math.ceil(string.len(content) / 60)
---     else
---       height = height + 1
---     end
---   end
+  for _, content in ipairs(vim.api.nvim_buf_get_lines(buf, 0, -1, false)) do
+    local length = string.len(content)
+    if length > 0 then
+      height = height + math.ceil(string.len(content) / math.floor(vim.o.columns * 0.42))
+    else
+      height = height + 1
+    end
+  end
 
---   return height
--- end
+  return height
+end
 
 return {
   "rcarriga/nvim-notify",
@@ -35,18 +35,18 @@ return {
       return math.floor(vim.o.lines * 0.75)
     end,
     max_width = function()
-      return math.floor(vim.o.columns * 0.75)
+      return math.floor(vim.o.columns * 0.42)
     end,
     minimum_width = 5,
-    -- on_open = function(win)
-    --   if vim.api.nvim_win_is_valid(win) then
-    --     vim.api.nvim_win_set_option(win, "wrap", true)
-    --     vim.api.nvim_win_set_config(win, {
-    --       border = "solid",
-    --       height = get_notification_height(win),
-    --     })
-    --   end
-    -- end,
+    on_open = function(win)
+      if vim.api.nvim_win_is_valid(win) then
+        vim.api.nvim_win_set_option(win, "wrap", true)
+        vim.api.nvim_win_set_config(win, {
+          -- border = "solid",
+          height = get_notification_height(win),
+        })
+      end
+    end,
     render = "compact",
     stages = "fade_in_slide_out",
     timeout = 300,

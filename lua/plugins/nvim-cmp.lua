6d5c2cde46ce0a59e7b20cd4c4035009e38e874f
César Miran <csarmiran@gmail.com>
2023-08-28 20:13:36 +0200

    style: toggle cmp for nvim-cmp

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -24,51 +24,23 @@ return {
       local cmp = require("cmp")
       local luasnip = require("luasnip")
 
-      require("luasnip/loaders/from_vscode").lazy_load()
-
-      local check_backspace = function()
-        local col = vim.fn.col "." - 1
-        return col == 0 or vim.fn.getline("."):sub(col, col):match "%s"
+      local has_words_before = function()
+        unpack = unpack or table.unpack
+        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
       end
 
-      -- find more here: https://www.nerdfonts.com/cheat-sheet
-      local kind_icons = {
-        Text = "",
-        Method = "m",
-        Function = "",
-        Constructor = "",
-        Field = "",
-        Variable = "",
-        Class = "",
-        Interface = "",
-        Module = "",
-        Property = "",
-        Unit = "",
-        Value = "",
-        Enum = "",
-        Keyword = "",
-        Snippet = "",
-        Color = "",
-        File = "",
-        Reference = "",
-        Folder = "",
-        EnumMember = "",
-        Constant = "",
-        Struct = "",
-        Event = "",
-        Operator = "",
-        TypeParameter = "",
-      }
-
       return {
-        confirm_opts = {
-          behavior = cmp.ConfirmBehavior.Replace,
-          select = false,
+        completion = {
+          completeopt = "menu,menuone,noinsert",
         },
-        -- disable completion in certain contexts, such as comments
+        -- confirm = {
+        --   behavior = cmp.ConfirmBehavior.Replace,
+        --   select = false,
+        -- },
         -- enabled = function()
         --   -- disable completion in comments
-        --   local context = require "cmp.config.context"
+        --   local context = require("cmp.config.context")
         --   -- keep command mode completion enabled when cursor is in a comment
         --   if vim.api.nvim_get_mode().mode == "c" then
         --     return true
@@ -77,56 +49,50 @@ return {
         --   end
         -- end,
         experimental = {
-          ghost_text = false,
-          native_menu = false,
+          ghost_text = {
+            hl_group = "LspCodeLens",
+          },
         },
         formatting = {
-          fields = { "kind", "abbr", "menu" },
-          format = function(entry, vim_item)
-            -- Kind icons
-            vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
-            -- vim_item.kind = string.format("%s %s", kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
-            vim_item.menu = ({
-              nvim_lsp = "lsp",
-              nvim_lua = "lsp",
-              cmdline = "lsp",
-              luasnip = "snippet",
-              buffer = "buffer",
-              path = "path",
-            })[entry.source.name]
-            return vim_item
+          format = function(_, item)
+            local icons = require("util").icons.kinds
+            if icons[item.kind] then
+              item.kind = icons[item.kind] .. item.kind
+            end
+            return item
           end,
         },
         mapping = {
           ["<C-k>"] = cmp.mapping.select_prev_item(),
           ["<C-j>"] = cmp.mapping.select_next_item(),
-          ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
-          ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
-          ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+          ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1)),
+          ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1)),
+          ["<C-Space>"] = cmp.mapping(cmp.mapping.complete()),
           ["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
           ["<C-e>"] = cmp.mapping {
             i = cmp.mapping.abort(),
             c = cmp.mapping.close(),
           },
-          -- Accept currently selected item. If none selected, `select` first item.
           -- Set `select` to `false` to only confirm explicitly selected items.
-          ["<CR>"] = cmp.mapping.confirm { select = false },
+          ["<CR>"] = cmp.mapping.confirm { select = true },
+          -- Set `select` to `false` to only confirm explicitly selected items.
+          ["<S-CR>"] = cmp.mapping.confirm({
+            behavior = cmp.ConfirmBehavior.Replace,
+            select = true,
+          }),
           ["<Tab>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
               cmp.select_next_item()
-            elseif luasnip.expandable() then
-              luasnip.expand()
+              -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
+              -- they way you will only jump inside the snippet region
             elseif luasnip.expand_or_jumpable() then
               luasnip.expand_or_jump()
-            elseif check_backspace() then
-              fallback()
+            elseif has_words_before() then
+              cmp.complete()
             else
               fallback()
             end
-          end, {
-            "i",
-            "s",
-          }),
+          end, { "i", "s" }),
           ["<S-Tab>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
               cmp.select_prev_item()
@@ -135,10 +101,7 @@ return {
             else
               fallback()
             end
-          end, {
-            "i",
-            "s",
-          }),
+          end, { "i", "s" }),
         },
         snippet = {
           expand = function(args)
@@ -147,10 +110,14 @@ return {
         },
         sorting = {
           comparators = {
+            -- Below is the default comparator list and order for nvim-cmp
             cmp.config.compare.offset,
+            -- cmp.config.compare.scopes, -- this is commented in nvim-cmp
             cmp.config.compare.exact,
             cmp.config.compare.score,
             require("cmp-under-comparator").under,
+            cmp.config.compare.recently_used,
+            cmp.config.compare.locality,
             cmp.config.compare.kind,
             cmp.config.compare.sort_text,
             cmp.config.compare.length,
@@ -165,17 +132,10 @@ return {
           { name = "buffer" },
           { name = "path" },
         },
-        view = {
-          entries = {
-            name = "custom",
-            selection_order = "near_cursor"
-          },
-        },
-        window = {
-          documentation = {
-            border = "none" -- { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
-          }
-        },
+        -- window = {
+        --   completion = cmp.config.window.bordered(),
+        --   documentation = cmp.config.window.bordered(),
+        -- },
       }
     end,
   },

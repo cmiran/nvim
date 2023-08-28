55657e544ce06eaa40763bc8250d6c33f533b216
César Miran <csarmiran@gmail.com>
2023-08-28 21:37:10 +0200

    chore(autocmds): add multiple new autocommands

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,44 +1,77 @@
-local create_autocmd = vim.api.nvim_create_autocmd
-
--- Quit window with 'q' or 'ESC' on filetype.
-create_autocmd({ "FileType" }, {
+-- quit window with 'q' or 'ESC' on filetype
+vim.api.nvim_create_autocmd("FileType", {
   pattern = {
-    "qf",
+    "checkhealth",
     "help",
     "lspinfo",
-    "notify",
     "man",
     "Markdown",
+    "notify",
+    "PlenaryTestPopup",
+    "qf",
+    "startuptime",
   },
-  callback = function()
-    vim.cmd [[
-      nnoremap <silent> <buffer> q :close<cr>
-      nnoremap <silent> <buffer> <esc> :close<cr>
-      set nobuflisted
-    ]]
+  callback = function(event)
+    vim.bo[event.buf].buflisted = false
+    vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
+    vim.keymap.set("n", "<esc>", "<cmd>close<cr>", { buffer = event.buf, silent = true })
   end,
 })
 
-create_autocmd({ "TextYankPost" }, {
+-- check if we need to reload the file when it changed
+vim.api.nvim_create_autocmd({ "FocusGained", "TermClose", "TermLeave" }, {
+  command = "checktime",
+})
+
+-- highlight on yank
+vim.api.nvim_create_autocmd({ "TextYankPost" }, {
   callback = function()
-    vim.highlight.on_yank { higroup = "Visual", timeout = 200 }
+    vim.highlight.on_yank({ higroup = "Visual", timeout = 200 })
   end,
 })
 
--- Reloads neovim whenever you save the plugins.lua file.
--- vim.cmd [[
---   augroup packer_user_config
---     autocmd!
---     autocmd BufWritePost plugins.lua source <afile> | PackerSync
---   augroup end
--- ]]
+-- resize splits if window got resized
+vim.api.nvim_create_autocmd({ "VimResized" }, {
+  callback = function()
+    vim.cmd("tabdo wincmd =")
+  end,
+})
 
--- Hide status bar filetype.
--- create_autocmd({ "User" }, {
---   pattern = { "AlphaReady" },
+-- go to last loc when opening a buffer
+vim.api.nvim_create_autocmd("BufReadPost", {
+  callback = function()
+    local mark = vim.api.nvim_buf_get_mark(0, '"')
+    local lcount = vim.api.nvim_buf_line_count(0)
+    if mark[1] > 0 and mark[1] <= lcount then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+    end
+  end,
+})
+
+-- wrap and check for spell in text filetypes
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "gitcommit", "markdown" },
+  callback = function()
+    vim.opt_local.wrap = true
+    vim.opt_local.spell = true
+  end,
+})
+
+-- auto create dir when saving a file, in case some intermediate directory does not exist
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+  callback = function(event)
+    if event.match:match("^%w%w+://") then
+      return
+    end
+    local file = vim.loop.fs_realpath(event.match) or event.match
+    vim.fn.mkdir(vim.fn.fnamemodify(file, ":p:h"), "p")
+  end,
+})
+
+-- change background color for help
+-- vim.api.nvim_create_autocmd({"FileType", "BufEnter"}, {
+--   pattern = { "help" },
 --   callback = function()
---     vim.cmd [[
---       set laststatus=0
---     ]]
+--     vim.cmd[[highlight Normal guifg=#cdcecf guibg=#131a24]]
 --   end,
 -- })

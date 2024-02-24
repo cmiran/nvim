240aa1ba7ac68a578f078fc55fa8a81d7bda1319
César Miran <csarmiran@gmail.com>
2024-02-24 17:46:29 +0100

    feat(gopher): add keymaps

--- lua/plugins/gopher.lua
+++ lua/plugins/gopher.lua
@@ -6,6 +6,53 @@ return {
     "nvim-treesitter/nvim-treesitter",
   },
   ft = "go",
+  keys = {
+    {
+      "<leader>cgd",
+      "<cmd>GoTagAdd db<cr>",
+      desc = "Add db tag",
+    },
+    {
+      "<leader>cge",
+      "<cmd>:GoTestsExp<cr>",
+      desc = "Generate if err",
+    },
+    {
+      "<leader>cgE",
+      "<cmd>:GoTestsExp<cr>",
+      desc = "Generate test only exported",
+    },
+    {
+      "<leader>cgi",
+      "<cmd>GoTagAdd ion<cr>",
+      desc = "Add ion tag",
+    },
+    {
+      "<leader>cgj",
+      "<cmd>GoTagAdd json<cr>",
+      desc = "Add json tag",
+    },
+    {
+      "<leader>cgm",
+      "<cmd>GoMod tidy<cr>",
+      desc = "Run go mod tidy",
+    },
+    {
+      "<leader>cgt",
+      "<cmd>:GoTestsAdd<cr>",
+      desc = "Generate test",
+    },
+    {
+      "<leader>cgT",
+      "<cmd>:GoTestsAll<cr>",
+      desc = "Generate all tests",
+    },
+    {
+      "<leader>cgy",
+      "<cmd>GoTagAdd yaml<cr>",
+      desc = "Add yaml tag",
+    },
+  },
   opts = {
     commands = {
       gotests = "~/.go/bin/gotests",

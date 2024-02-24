0c38fb5703f1c0e0a347a42b292063717018c948
César Miran <csarmiran@gmail.com>
2024-02-24 11:20:32 +0100

    feat(lualine): remove diagnostics

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -23,15 +23,15 @@ return {
         lualine_a = { "mode" },
         lualine_b = { "branch" },
         lualine_c = {
-          {
-            "diagnostics",
-            symbols = {
-              error = icons.diagnostics.Error,
-              warn = icons.diagnostics.Warn,
-              info = icons.diagnostics.Info,
-              hint = icons.diagnostics.Hint,
-            },
-          },
+          -- {
+          --   "diagnostics",
+          --   symbols = {
+          --     error = icons.diagnostics.Error,
+          --     warn = icons.diagnostics.Warn,
+          --     info = icons.diagnostics.Info,
+          --     hint = icons.diagnostics.Hint,
+          --   },
+          -- },
           { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
           { "filename", path = 1, symbols = { modified = "  ", readonly = "", unnamed = "" } },
           -- stylua: ignore

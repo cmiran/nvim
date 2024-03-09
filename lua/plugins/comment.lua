721af96f7c766bb382cfdac85bbaf821dfd34189
César Miran <csarmiran@gmail.com>
2024-03-09 15:59:45 +0100

    fix(comment): remove unknown lazy property

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -2,17 +2,12 @@ return {
   "numToStr/Comment.nvim",
   dependencies = {
     -- github.com/JoosepAlviste/nvim-ts-context-commentstring
-    "JoosepAlviste/nvim-ts-context-commentstring",
+    -- "JoosepAlviste/nvim-ts-context-commentstring",
     -- github.com/nvim-treesitter/nvim-treesitter
     "nvim-treesitter/nvim-treesitter",
   },
   lazy = false,
   opts = {
-    ignore = '^$', -- ignores empty lines
-  },
-  setup = {
-    pre_hook = function()
-      return vim.bo.commentstring
-    end
+    ignore = "^$", -- ignores empty lines
   },
 }

8fc161987d07c460e0c3682ba1074990cdc4ff8a
César Miran <csarmiran@gmail.com>
2025-04-08 21:29:05 +0200

    feat(autocmd): redraw diagnostics on mode change

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -83,6 +83,14 @@ vim.api.nvim_create_autocmd({ "BufWritePre" }, {
   end,
 })
 
+vim.api.nvim_create_autocmd('ModeChanged', {
+  desc = "redraw the diagnostics when the mode change",
+  group = vim.api.nvim_create_augroup('diagnostic_redraw', {}),
+  callback = function()
+    pcall(vim.diagnostic.show)
+  end
+})
+
 -- change background color for help
 -- vim.api.nvim_create_autocmd({"FileType", "BufEnter"}, {
 --   pattern = { "help" },
@@ -91,3 +99,36 @@ vim.api.nvim_create_autocmd({ "BufWritePre" }, {
 --     -- vim.api.nvim_win_set_hl(0, "Normal", { bg = "#131a24" })
 --   end,
 -- })
+--
+
+-- reddit.com/r/neovim/comments/1jpbc7s/disable_virtual_text_if_there_is_diagnostic_in/
+-- vim.api.nvim_create_autocmd({ 'CursorMoved', 'DiagnosticChanged' }, {
+--      group = vim.api.nvim_create_augroup('diagnostic_virt_text_hide', {}),
+--   callback = function(ev)
+--     local lnum, _ = unpack(vim.api.nvim_win_get_cursor(0))
+--     lnum = lnum - 1 -- need 0-based index
+
+--    local hidden_lnum = vim.b[ev.buf].diagnostic_hidden_lnum
+--    if hidden_lnum and hidden_lnum ~= lnum then
+--      vim.b[ev.buf].diagnostic_hidden_lnum = nil
+--      -- display all the decorations if the current line changed
+--      vim.diagnostic.show(nil, ev.buf)
+--    end
+
+--    for _, namespace in pairs(vim.diagnostic.get_namespaces()) do
+--      local ns_id = namespace.user_data.virt_text_ns
+--        if ns_id then
+--          local extmarks = vim.api.nvim_buf_get_extmarks(ev.buf, ns_id, { lnum, 0 }, { lnum, -1 }, {})
+--          for _, extmark in pairs(extmarks) do
+--            local id = extmark[1]
+--            vim.api.nvim_buf_del_extmark(ev.buf, ns_id, id)
+--          end
+
+--          if extmarks and not vim.b[ev.buf].diagnostic_hidden_lnum then
+--            vim.b[ev.buf].diagnostic_hidden_lnum = lnum
+--         end
+--       end
+--     end
+--   end,
+-- })
+

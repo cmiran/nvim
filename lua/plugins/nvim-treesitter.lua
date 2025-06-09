d9ba3256d49f6f49f5dbafdf2f537cc0bd5fe852
César Miran <csarmiran@gmail.com>
2025-06-09 13:28:00 +0200

    feat(treesitter): enable incremental selection and highlight

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -12,11 +12,9 @@ return {
     -- github.com/p00f/nvim-ts-rainbow
     "p00f/nvim-ts-rainbow",
   },
-  keys = {
-    { "<c-space>", desc = "Increment selection" },
-    { "<bs>", desc = "Decrement selection", mode = "x" },
-  },
   opts = {
+    auto_install = true,
+    sync_install = false,
     ensure_installed = {
       "bash",
       "c",
@@ -49,21 +47,6 @@ return {
       "vim",
       "yaml",
     },
-    sync_install = false, -- Install parsers synchronously (only applied to `ensure_installed`)
-    auto_install = true, -- Automatically install missing parsers when entering buffer
-    ignore_install = { "" }, -- List of parsers to ignore installing
-    highlight = {
-      enable = true, -- false will disable the whole extension
-      -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
-      -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
-      -- the name of the parser).
-      disable = { "" }, -- list of language that will be disabled
-      -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-      -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-      -- Using this option may slow down your editor, and you may see some duplicate highlights.
-      -- Instead of true it can also be a list of languages.
-      additional_vim_regex_highlighting = false,
-    },
     autopairs = {
       enable = true,
     },
@@ -71,27 +54,29 @@ return {
       enable = true,
       enable_autocmd = false,
     },
+    highlight = {
+      enable = true,
+      additional_vim_regex_highlighting = false,
+    },
     indent = {
-      enable = false,
-      disable = { "" },
+      enable = true,
+    },
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = "<Leader>cs",
+        node_incremental = "+",
+        scope_incremental = false,
+        node_decremental = "-",
+      },
     },
     rainbow = {
       enable = false,
-      -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
       extended_mode = true, -- also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
       max_file_lines = nil, -- do not enable for files with more than n lines, int
       -- colors = {}, -- table of hex strings
       -- termcolors = {} -- table of colour name strings
     },
-    -- incremental_selection = {
-    --   enable = true,
-    --   keymaps = {
-    --     init_selection = "<C-space>",
-    --     node_incremental = "<C-space>",
-    --     scope_incremental = false,
-    --     node_decremental = "<bs>",
-    --   },
-    -- },
     -- refactor = {
     -- highlight_definitions = {
     --   enable = true,

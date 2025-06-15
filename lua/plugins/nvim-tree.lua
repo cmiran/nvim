8fd9a333297a838e8f080d3a585f3c9d0287b6e4
César Miran <csarmiran@gmail.com>
2025-06-15 18:12:56 +0200

    feat(nvim-tree): move tree to the right

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -24,6 +24,17 @@ local function on_attach(bufnr)
   )
 end
 
+local function win_config()
+  return {
+    relative = "editor",
+    border = "shadow",
+    width = math.floor(vim.o.columns * 0.4),
+    height = math.floor(vim.o.lines * 0.7),
+    row = 6,
+    col = math.floor(vim.o.columns * 0.565),
+  }
+end
+
 return {
   -- github.com/kyazdani42/nvim-tree.lua/
   "nvim-tree/nvim-tree.lua",
@@ -39,62 +50,69 @@ return {
     "NvimTreeToggle",
   },
   keys = {
-    { "<leader>e", "<cmd>NvimTreeToggle<cr>", desc = "tree" },
-    { "<leader>m", "<cmd>NvimTreeOpen /tmp<cr>", desc = "tree /tmp" },
+    { "<leader>e", "<cmd>NvimTreeToggle<cr>",    desc = "Tree" },
+    { "<leader>m", "<cmd>NvimTreeOpen /tmp<cr>", desc = "Tree /tmp" },
   },
-  opts = function()
-    return {
-      on_attach = on_attach,
-      disable_netrw = true,
-      sync_root_with_cwd = true,
-      view = {
-        side = "left",
-        float = {
-          enable = true,
-          open_win_config = {
-            relative = "editor",
-            border = "shadow",
-            width = 32,
-            -- height = math.floor(vim.o.lines * 0.7) - 5,
-            row = 6,
-            col = 5,
-          },
-        },
+  opts = {
+    on_attach = on_attach,
+    disable_netrw = true,
+    sync_root_with_cwd = true,
+    view = {
+      preserve_window_proportions = true,
+      float = {
+        enable = true,
+        open_win_config = win_config
       },
-      renderer = {
-        highlight_git = true,
-        root_folder_modifier = ":t", -- ":~:s?$?/..?"
-        indent_markers = {
-          enable = false,
-          icons = { corner = "│" },
-        },
+    },
+    renderer = {
+      highlight_git = true,
+      root_folder_modifier = ":t", -- ":~:s?$?/..?"
+      indent_markers = {
+        enable = false,
         icons = {
-          -- git_placement = "after",
-          show = {
-            git = false,
-          },
+          corner = "│",
         },
       },
-      update_focused_file = {
-        enable = true,
-        update_root = true,
-      },
-      diagnostics = {
-        enable = true,
-        show_on_dirs = true,
-        debounce_delay = 20,
-      },
-      actions = {
-        open_file = {
-          quit_on_open = true,
-          window_picker = {
-            chars = "123456789QWERTYUIOPASDFGHJKLZXCVBNM",
-          },
+      icons = {
+        show = {
+          git = false,
         },
       },
-      git = {
-        ignore = false,
+    },
+    update_focused_file = {
+      enable = true,
+      update_root = true,
+    },
+    diagnostics = {
+      enable = true,
+      show_on_dirs = true,
+      debounce_delay = 20,
+    },
+    actions = {
+      open_file = {
+        quit_on_open = true,
+        window_picker = {
+          chars = "123456789QWERTYUIOPASDFGHJKLZXCVBNM",
+        },
       },
-    }
+    },
+    git = {
+      ignore = false,
+    },
+  },
+  config = function(_, opts)
+    vim.api.nvim_create_autocmd("VimResized", {
+      callback = function()
+        local view = require("nvim-tree.view")
+        if not view.is_visible() then return end
+
+        local win = view.get_winnr()
+        if not vim.api.nvim_win_is_valid(win) then return end
+
+        pcall(vim.api.nvim_win_set_config, win, win_config())
+      end,
+    })
+
+    require("nvim-tree").setup(opts)
   end,
 }
